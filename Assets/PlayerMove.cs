8dcbacf96865ae557bacac29baca8c55fd0bbcd0
akshaya-ranjit <99360446+akshaya-ranjit@users.noreply.github.com>
2024-11-06 17:41:22 -0600

    adding Akshaya's stuff (tilemap)

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
 
 public class PlayerMove : MonoBehaviour
 {
-    public float speed = 1;
+    public float speed = 10;
     public Transform targetPosition;
     private Vector2 movement;
-
+    public Tilemap map;
     public LayerMask UnwalkableLayer;
     public LayerMask MoveableLayer;
 
@@ -20,32 +21,48 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*if (Vector3.Distance(transform.position, targetPosition.position) < 0.1f &&
-            !Physics2D.OverlapCircle(targetPosition.position + new Vector3(movement.x, movement.y, 0f), .1f, UnwalkableLayer))
+        //if the target and transforms 
+        // changed overlap circle diameter to like .9
+        if (Vector3.Distance(transform.position, targetPosition.position) < 0.1f &&
+            !Physics2D.OverlapCircle(targetPosition.position + new Vector3(movement.x, movement.y, 0f), 1f, UnwalkableLayer))
         {
-            if (Physics2D.OverlapCircle(targetPosition.position + new Vector3(movement.x, movement.y, 0f), .1f, MoveableLayer))
+            Debug.Log("bojangles was here. aka I should be able to movein the direction I'm trying to");
+            Debug.Log(targetPosition.position);
+            //changed overlap circle from .1f to .2f because it's not registering when I'm one unit away from a moveable object
+            Collider2D g = Physics2D.OverlapCircle(targetPosition.position + new Vector3(movement.x, movement.y, 0f), .2f, MoveableLayer);
+            if (g) // there is a moveable gameObject next to me
             {
+                //this might need to be an if-else statement eventually
+                //for smooth movement (not blinking), give g its own target position and movetowards
+                Debug.Log("there's a moveable object next to me, imma try to move it");
+                g.gameObject.transform.position = new Vector3(g.gameObject.transform.position.x + movement.x, g.gameObject.transform.position.y + movement.y, 0f);
+                //if the thing we're trying to move isn't also next to something that shouldn't move
                 if (!Physics2D.OverlapCircle(targetPosition.position + new Vector3(2 * movement.x, 2 * movement.y, 0f), .1f, UnwalkableLayer))
                 {
+                    Debug.Log("I should be able to move it");
                     targetPosition.position = new Vector3(targetPosition.position.x + movement.x, targetPosition.position.y + movement.y, 0f);
                 }
             }
             else
             {
+                //should be able to move freely
+                Debug.Log("no obstacles to movement");
                 targetPosition.position = new Vector3(targetPosition.position.x + movement.x, targetPosition.position.y + movement.y, 0f);
             }
         }
-        targetPosition.position = new Vector3(targetPosition.position.x + movement.x, targetPosition.position.y + movement.y, 0f);
-        transform.position = Vector3.MoveTowards(transform.position, targetPosition.position, speed * Time.deltaTime);*/
+        //targetPosition.position = new Vector3(targetPosition.position.x + movement.x, targetPosition.position.y + movement.y, 0f);
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition.position, speed * Time.deltaTime);
     }
     private void OnMove(InputValue value)
     {
+        
         movement = value.Get<Vector2>();
+        Debug.Log(movement);
         /*if (movement.x != 0 && movement.y != 0)
         {
             movement = new Vector2(0, 0);
         }*/
-        if (Vector3.Distance(transform.position, targetPosition.position) < 0.1f)
+        /*if (Vector3.Distance(transform.position, targetPosition.position) < 0.1f)
         {
             targetPosition.position = new Vector3(targetPosition.position.x + movement.x, targetPosition.position.y + movement.y, 0f);
             //transform.position = Vector3.MoveTowards(transform.position, targetPosition.position, speed * Time.deltaTime);
@@ -55,7 +72,7 @@ public class PlayerMove : MonoBehaviour
         {
             targetPosition.position = new Vector3(transform.position.x + movement.x, transform.position.y + movement.y, 0f);
             transform.position = targetPosition.position;
-        }
+        }*/
     }
     /*
     public void Update()

7d79c1472500cb6cef39c806e1a74e3a69bad1fe
akshaya-ranjit <99360446+akshaya-ranjit@users.noreply.github.com>
2024-11-21 20:59:14 -0600

    updated with UI working

--- Assets/Level2_Room/NovelScript/GameManaging.cs
+++ Assets/Level2_Room/NovelScript/GameManaging.cs
@@ -4,30 +4,49 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
 
 
 
 public class GameManaging : MonoBehaviour
 {
+    public Transform player;
     public Light2D global;
     public Light2D playerLight;
     public Collider2D compTrigger;
+    public SpriteRenderer closetIcon;
+    public LayerMask ClosetPcs;
+    public GameObject finCloset;
+    public Button closetBuilder;
+    public GameObject closetPuzz;
+
     //public UnityOnTriggerEnter2DMessageListener compTrigger2;
     // Start is called before the first frame update
     void Start()
     {
         global.gameObject.SetActive(false);
         playerLight.gameObject.SetActive(true);
+        closetIcon.color = new Color (0,0,0,1);
+        closetBuilder.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Debug.Log(player.position);
+        if(Physics2D.OverlapCircle(player.position, 2f, ClosetPcs)){
+            closetIcon.color = new Color (255, 255 , 255 ,255);
+            closetBuilder.gameObject.SetActive(true);
+        }
     }
 
     public void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("player triggered event");
     }
+
+    public void puzzleVisible(){
+        closetPuzz.SetActive(true); 
+    }
+
 }
